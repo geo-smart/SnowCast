4290214558777311e31ee291ccd76084e9b5c46a
Ziheng Sun <zsun@gmu.edu>
2024-01-07 16:16:24 -0500

    latest version of working code in the snow wormhole operational service

---
+++ code/fSCA_training_extract_data.py
@@ -0,0 +1,173 @@
+import os
+import pandas as pd
+import rasterio
+from pyproj import Transformer
+from rasterio.enums import Resampling
+import concurrent.futures
+from snowcast_utils import homedir, work_dir, train_start_date, train_end_date
+from datetime import datetime, timedelta
+import dask.dataframe as dd
+
+working_dir = f"{homedir}/fsca"
+folder_path = f"{working_dir}/final_output/"
+new_base_station_list_file = f"{work_dir}/all_snotel_cdec_stations_active_in_westus.csv"
+cell_to_modis_mapping = f"{working_dir}/training_cell_to_modis_mapper_original_snotel_stations.csv"
+modis_day_wise = f"{working_dir}/final_output/"
+os.makedirs(modis_day_wise, exist_ok=True)
+
+
+def map_modis_to_station(row, src):
+  drow, dcol = src.index(row["lon"], row["lat"])
+  return drow, dcol
+
+def prepare_modis_grid_mapper_training():
+  # actually, not sure this applied for modis. The tile HDF must be exactly same extent to make this work. Otherwise, the mapper won't get usable. 
+  
+  if os.path.exists(cell_to_modis_mapping):
+    print(f"The file {cell_to_modis_mapping} exists. skip.")
+  else:
+    print(f"start to generate {cell_to_modis_mapping}")
+    station_df = pd.read_csv(new_base_station_list_file)
+    print("original station_df describe() = ", station_df.describe())
+
+    sample_modis_tif = f"{modis_day_wise}/2022-10-01__snow_cover.tif"
+
+    with rasterio.open(sample_modis_tif) as src:
+      # Apply get_band_value for each row in the DataFrame
+      #station_df['modis_y'], station_df['modis_x'] = zip(*station_df.apply(map_modis_to_station, axis=1, args=(src,)))
+      print("Spatial Extent (Bounding Box):", src.bounds)
+      # Get the affine transformation matrix
+      transform = src.transform
+
+      # Extract the spatial extent using the affine transformation
+      left, bottom, right, top = rasterio.transform.array_bounds(src.height, src.width, transform)
+
+      # Print the spatial extent
+      print("Spatial Extent (Bounding Box):", (left, bottom, right, top))
+      
+      station_df['modis_y'], station_df['modis_x'] = rasterio.transform.rowcol(
+        src.transform, 
+        station_df["longitude"], 
+        station_df["latitude"])
+      
+      # print(f"Saving mapper csv file: {cell_to_modis_mapping}")
+      station_df.to_csv(cell_to_modis_mapping, index=False, columns=['latitude', 'longitude', 'modis_x', 'modis_y'])
+      
+      print("after mapped modis station_df.describe() = ", station_df.describe())
+
+
+def get_value_at_coords(src, lat, lon, band_number=1):
+#     transformer = Transformer.from_crs("EPSG:4326", src.crs, always_xy=True)
+#     east, north = transformer.transform(lon, lat)
+  if not (src.bounds.left <= lat <= src.bounds.right and src.bounds.bottom <= lon <= src.bounds.top):
+    return None
+  row, col = src.index(lat, lon)
+  if (0 <= row < src.height) and (0 <= col < src.width):
+    return src.read(band_number, window=((row, row+1), (col, col+1)), resampling=Resampling.nearest)[0, 0]
+  else:
+    return None
+
+def get_band_value(row, src):
+  if (row["modis_y"] < src.height) and (row["modis_x"] < src.width):
+    # print("src.height = ", src.height, " - ", row["modis_y"])
+    # print("src.width = ", src.width, " - ", row["modis_x"])
+    # print(row)
+    valid_value =  src.read(1, 
+                            window=((row["modis_y"],
+                                     row["modis_y"]+1), 
+                                    (row["modis_x"],
+                                     row["modis_x"]+1)))
+    # print("valid_value[0,0] = ", valid_value[0,0])
+    return valid_value[0,0]
+  else:
+    return None
+          
+def process_file(file_path, current_date_str, outfile):
+  station_df = pd.read_csv(cell_to_modis_mapping)
+  # print("station_df.head() = ", station_df.head())
+
+  # Apply get_band_value for each row in the DataFrame
+  with rasterio.open(file_path) as src:
+    # Apply get_band_value for each row in the DataFrame
+    # Get the affine transformation matrix
+    transform = src.transform
+
+    # Extract the spatial extent using the affine transformation
+    left, bottom, right, top = rasterio.transform.array_bounds(src.height, src.width, transform)
+
+    # Print the spatial extent
+    # print("Spatial Extent (Bounding Box):", (left, bottom, right, top))
+
+    station_df['fsca'] = station_df.apply(get_band_value, axis=1, args=(src,))
+
+    
+  # Prepare final data
+  station_df['date'] = current_date_str
+  station_df.to_csv(outfile, index=False, 
+                    columns=['date', 'latitude', 'longitude', 'fsca'])
+  print(f"Saved to csv: {outfile}")
+
+def merge_csv(start_date, end_date):
+  import glob
+  # Find CSV files within the specified date range
+  csv_files = glob.glob(folder_path + '*_training_output_station_corrected.csv')
+  relevant_csv_files = []
+
+  for c in csv_files:
+    # Extract the date from the file name
+    # print("c = ", c)
+    file_name = os.path.basename(c)
+    date_str = file_name.split('_')[0]  # Assuming the date is part of the file name
+    # print("date_str = ", date_str)
+    file_date = datetime.strptime(date_str, "%Y-%m-%d")
+
+    # Check if the file date is within the specified range
+    if start_date <= file_date <= end_date:
+      relevant_csv_files.append(c)
+#       # Read and concatenate only relevant CSV files
+#       df = []
+#       for c in relevant_csv_files:
+#         tmp = pd.read_csv(c, low_memory=False, usecols=['date', 'latitude', 'longitude', 'fsca'])
+#         df.append(tmp)
+
+#         combined_df = pd.concat(df, ignore_index=True)
+
+  # Initialize a Dask DataFrame
+  print("start to use dask to read all csv files")
+  dask_df = dd.read_csv(relevant_csv_files)
+
+  # Save the merged DataFrame to a CSV file
+  output_file = f'{working_dir}/fsca_final_training_all.csv'
+  # Write the Dask DataFrame to a single CSV file
+  print(f"saving all csvs into one file: {output_file}")
+  dask_df.to_csv(output_file, index=False, single_file=True)
+  #combined_df.to_csv(output_file, index=False)
+
+  #print(combined_df.describe())
+  print(f"Merged data saved to {output_file}")
+  
+def main():
+  
+  start_date = datetime.strptime(train_start_date, "%Y-%m-%d")
+  
+  end_date = datetime.strptime(train_end_date, "%Y-%m-%d")
+  
+  prepare_modis_grid_mapper_training()
+  
+  date_list = [start_date + timedelta(days=i) for i in range((end_date - start_date).days + 1)]
+  for i in date_list:
+    current_date = i.strftime("%Y-%m-%d")
+    #print(f"extracting data for {current_date}")
+    outfile = os.path.join(modis_day_wise, f'{current_date}_training_output_station_corrected.csv')
+    if os.path.exists(outfile):
+      #print(f"The file {outfile} exists. skip.")
+      pass
+    else:
+      process_file(f'{modis_day_wise}/{current_date}__snow_cover.tif', current_date, outfile)
+  
+  merge_csv(start_date, end_date)
+
+if __name__ == "__main__":
+  main()
+  print("fsca Data extraction complete.")
+
--- code/merge_custom_traning_range.py
+++ code/merge_custom_traning_range.py
@@ -16,17 +16,49 @@ Functions:
 
 import dask.dataframe as dd
 import os
-from snowcast_utils import work_dir
+from snowcast_utils import work_dir, homedir
+import pandas as pd
 
 working_dir = work_dir
-chunk_size = '32MB'  # You can adjust this chunk size based on your hardware and data size
+final_output_name = "final_merged_data_3yrs_all_active_stations_v1.csv"
+chunk_size = '10MB'  # You can adjust this chunk size based on your hardware and data size
 
-def main():
+
+def merge_all_data_together():
+    amsr_file = f'{working_dir}/all_snotel_cdec_stations_active_in_westus.csv_amsr_dask.csv'
+    snotel_file = f'{working_dir}/all_snotel_cdec_stations_active_in_westus.csv_swe_restored_dask_all_vars.csv'
+    gridmet_file = f'{working_dir}/training_all_active_snotel_station_list_elevation.csv_gridmet.csv'
+    terrain_file = f'{working_dir}/training_all_active_snotel_station_list_elevation.csv_terrain_4km_grid_shift.csv'
+    fsca_file = f'{homedir}/fsca/fsca_final_training_all.csv'
+    final_final_output_file = f'{work_dir}/{final_output_name}'
+    
+    if os.path.exists(final_final_output_file):
+      print(f"The file '{final_final_output_file}' exists. Skipping")
+      return final_final_output_file
+      
+    
     # Read the CSV files with a smaller chunk size and compression
-    amsr = dd.read_csv(f'{working_dir}/training_ready_amsr_3_yrs.csv', blocksize=chunk_size)
-    snotel = dd.read_csv(f'{working_dir}/training_data_ready_snotel_3_yrs.csv', blocksize=chunk_size)
-    gridmet = dd.read_csv(f'{working_dir}/gridmet_climatology/training_ready_gridmet.csv', blocksize=chunk_size)
-    terrain = dd.read_csv(f'{working_dir}/training_ready_terrain.csv', blocksize=chunk_size)
+    amsr = dd.read_csv(amsr_file, blocksize=chunk_size)
+    print("amsr.columns = ", amsr.columns)
+    snotel = dd.read_csv(snotel_file, blocksize=chunk_size)
+    print("snotel.columns = ", snotel.columns)
+#     gridmet = dd.read_csv(f'{working_dir}/gridmet_climatology/training_ready_gridmet.csv', blocksize=chunk_size)
+    gridmet = dd.read_csv(gridmet_file, blocksize=chunk_size)
+    gridmet = gridmet.drop(columns=["Unnamed: 0"])
+    print("gridmet.columns = ", gridmet.columns)
+    terrain = dd.read_csv(terrain_file, blocksize=chunk_size)
+    terrain = terrain.rename(columns={
+      "latitude": "lat", 
+      "longitude": "lon"
+    })
+    terrain = terrain[["stationTriplet", "elevation", "lat", "lon", 'Elevation', 'Slope', 'Aspect', 'Curvature', 'Northness', 'Eastness']]
+    print("terrain.columns = ", terrain.columns)
+    snowcover = dd.read_csv(fsca_file, blocksize=chunk_size)
+    snowcover = snowcover.rename(columns={
+      "latitude": "lat", 
+      "longitude": "lon"
+    })
+    print("snowcover.columns = ", snowcover.columns)
 
     # Repartition DataFrames for optimized processing
     amsr = amsr.repartition(partition_size=chunk_size)
@@ -34,23 +66,66 @@ def main():
     gridmet = gridmet.repartition(partition_size=chunk_size)
     gridmet = gridmet.rename(columns={'day': 'date'})
     terrain = terrain.repartition(partition_size=chunk_size)
+    snow_cover = snowcover.repartition(partition_size=chunk_size)
+    print("all the dataframes are partitioned")
 
     # Merge DataFrames based on specified columns
+    print("start to merge amsr and snotel")
     merged_df = dd.merge(amsr, snotel, on=['lat', 'lon', 'date'], how='outer')
-    merged_df = dd.merge(merged_df, gridmet, on=['lat', 'lon', 'date'], how='outer')
-    merged_df = dd.merge(merged_df, terrain, on=['lat', 'lon'], how='outer')
-
-    # Save the merged DataFrame to a CSV file in chunks
-    output_file = os.path.join(working_dir, 'final_merged_data_3_yrs.csv')
+    merged_df = merged_df.drop_duplicates(keep='first')
+    output_file = os.path.join(working_dir, f"{final_output_name}_snotel.csv")
     merged_df.to_csv(output_file, single_file=True, index=False)
-    print('Merge completed.')
+    print(f"intermediate file saved to {output_file}")
+    
+    print("start to merge gridmet")
+    merged_df = dd.merge(merged_df, gridmet, on=['lat', 'lon', 'date'], how='outer')
+    merged_df = merged_df.drop_duplicates(keep='first')
+    output_file = os.path.join(working_dir, f"{final_output_name}_gridmet.csv")
+    merged_df.to_csv(output_file, single_file=True, index=False)
+    print(f"intermediate file saved to {output_file}")
+    
+    print("start to merge terrain")
+    merged_df = dd.merge(merged_df, terrain, on=['lat', 'lon'], how='outer')
+    merged_df = merged_df.drop_duplicates(keep='first')
+    output_file = os.path.join(working_dir, f"{final_output_name}_terrain.csv")
+    merged_df.to_csv(output_file, single_file=True, index=False)
+    print(f"intermediate file saved to {output_file}")
+    
+    print("start to merge snowcover")
+    merged_df = dd.merge(merged_df, snow_cover, on=['lat', 'lon', 'date'], how='outer')
+    merged_df = merged_df.drop_duplicates(keep='first')
+    output_file = os.path.join(working_dir, f"{final_output_name}_snow_cover.csv")
+    merged_df.to_csv(output_file, single_file=True, index=False)
+    print(f"intermediate file saved to {output_file}")
+    
+    # Save the merged DataFrame to a CSV file in chunks
+    output_file = os.path.join(working_dir, final_output_name)
+    merged_df.to_csv(output_file, single_file=True, index=False)
+    print(f'Merge completed. {output_file}')
 
     # Read the merged DataFrame, remove duplicate rows, and save the cleaned DataFrame to a new CSV file
-    df = dd.read_csv(f'{work_dir}/final_merged_data_3_yrs.csv')
+    df = dd.read_csv(f'{work_dir}/{final_output_name}')
     df = df.drop_duplicates(keep='first')
-    df.to_csv(f'{work_dir}/final_merged_data_3yrs_cleaned_v2.csv', single_file=True, index=False)
+    df.to_csv(f'{work_dir}/{final_output_name}', single_file=True, index=False)
     print('Data cleaning completed.')
+    return final_final_output_file
 
+  
+def sort_training_data(input_training_csv, sorted_training_csv):
+    # Read Dask DataFrame from CSV with increased blocksize and assuming missing data
+    ddf = dd.read_csv(input_training_csv, assume_missing=True, blocksize='10MB')
+
+    # Persist the Dask DataFrame in memory
+    ddf = ddf.persist()
+
+    # Sort Dask DataFrame by three columns: date, lat, and Lon
+    sorted_ddf = ddf.sort_values(by=['date', 'lat', 'lon'])
+
+    # Save the sorted Dask DataFrame to a new CSV file
+    sorted_ddf.to_csv(sorted_training_csv, index=False, single_file=True)
+    print(f"sorted training data is saved to {sorted_training_csv}")
+  
 if __name__ == "__main__":
-    main()
-
+    merge_all_data_together()
+    final_final_output_file = f'{work_dir}/{final_output_name}'
+    sort_training_data(final_final_output_file, f'{work_dir}/{final_output_name}_sorted.csv')
